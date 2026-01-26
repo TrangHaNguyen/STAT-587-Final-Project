2dc715670c991c03d2b06e578539f6fb3f76f878
Takeshi Stormer <takeshi.stormer@gmail.com>
2026-01-25 18:02:24 -0800

    Added requirements.txt which is utilized with the; updated main_project.cpp to automatically set up a python environment with the necessary packages(untested with the Windows OS); data_manager.py now has all the necessary tickers to start downloading data.

--- PyScripts/data_manager.py
+++ PyScripts/data_manager.py
@@ -1,2 +1,46 @@
 #!/usr/bin/env python3
 import yfinance
+import sys
+
+# ------------------------------------------------------------------------------------------------------------------
+# Retrieving the following data will utilize
+# - S&P 500 stocks
+# - Indexes:
+#   - S&P 500 (^SPX),
+#   - Russel 2000 (^RUT), 
+#   - NASDAQ (^IXIC),
+#   - Dow Jones Industrial (^DJI),
+#   - CBOE Volatility Index (^VIX),
+#   - Nikkei 225 (^N225) [Japan],
+#   - DAX (^GDAXI) [Germany].
+# - Commodities
+#   - Crude Oil (CL=F),
+#   - Natural Gas (NG=F),
+#   - Gold (GC=F),
+#   - Silver (SI=F), 
+#   - Corn (ZC=F)
+#
+# This data will be formatted as such: 
+# | Index (Date) | Stock 1 | ... | Stock 500 | Index 1 | ... | Index 7 | Commodity 1 | ... | Commodity 5 | 
+# | Date 0       |         |     |           |         |     |         |             |     |             |
+# | Date 1       |         |     |           |         |     |         |             |     |             |
+# |   .          |         |     |           |         |     |         |             |     |             |
+# |   .          |         |     |           |         |     |         |             |     |             |
+# | Date n       |         |     |           |         |     |         |             |     |             |
+#
+# With a stock lookup table as such:
+# | Stock (Name) | Sectors | Market Capitalization (in Mil.) |
+# | Stock 1      |         |                                 |
+# |   .          |         |                                 |
+# |   .          |         |                                 |
+# | Stock 500    |         |                                 |
+# ------------------------------------------------------------------------------------------------------------------
+
+print(f"Running: {sys.argv[0]}")
+TICKERS=sys.argv[1:]
+print("Retrieved TICKERS.")
+
+print("Setting up necessary variables...")
+INDEXES=["^SPX", "^RUT", "^IXIC", "^DJI", "^VIX", "^N225", "^GDAXI"]
+COMMODS=["CL=F", "NG=F", "GC=F", "SI=F", "ZC=F"]
+print("Finished setting up necessary variables.")
--- src/main_project.cpp
+++ src/main_project.cpp
@@ -1,7 +1,37 @@
 #include<retrieve_tickers.h>
 #include<iostream>
+#include<filesystem>
+#include<sstream>
 
 int main() {
+    // Ensuring python environment exists.
+    std::filesystem::path cwd = std::filesystem::current_path();
+    #ifdef _WIN32
+        std::filesystem::path env_dir = std::filesystem::current_path() / "PyScripts" / "env";
+        std::filesystem::path env_pyt = env_dir / "Scripts" / "python.exe";
+    #else
+        std::filesystem::path env_dir = std::filesystem::current_path() / "PyScripts" / "env";
+        std::filesystem::path env_pyt = env_dir / "bin" / "python3";
+    #endif
+    if (!std::filesystem::exists(env_pyt)) {
+        std::cout << "Initializing virtual environment...\n";
+        std::system("python -m venv PyScripts/env");
+        if (std::filesystem::exists("PyScripts/requirements.txt")) {
+            std::cout << "Downloading dependencies...\n";
+            std::system("PyScripts/env/bin/pip install -r PyScripts/requirements.txt");
+        }
+        std::cout << "Finished!\n";
+    }
+
     std::vector<std::string> TICKERS = retrieve_tickers();
+
+    // env_pyt does not require .string() and in-place quotation marks because it contains the entire path we are looking for, 
+    // whereas we are adding the current working directing (cwd) to the script of interest.
+    std::stringstream ss;
+    ss << env_pyt << " \"" << cwd.string() << "/PyScripts/data_manager.py\" ";
+    for (int i = 0; i < TICKERS.size(); i++) ss << TICKERS[i] << " ";
+    std::system(ss.str().c_str());
+
+
     return 1;
 }

ad4bfe94f012f8736ccd2505e96eb5f7932b0df6
Sabuchan123 <takeshi.stormer@gmail.com>
2026-02-09 21:34:09 -0800

    Updated .gitignore; added PCA so; a generic reduced random forest regression and random forest classification model was created; started the workings of stepwise regression to select from principal componenets.

--- PyScripts/data_preprocessing.py
+++ PyScripts/data_preprocessing.py
@@ -5,12 +5,22 @@ import matplotlib.pyplot as plt
 from pathlib import Path
 from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from mlxtend.feature_selection import SequentialFeatureSelector as MFS
+from sklearn.model_selection import TimeSeriesSplit
 
 pd.set_option('display.max_rows', 100)
 pd.set_option('display.max_columns', 8)
 
 cwd=Path.cwd()
-while (cwd.name!="STAT-587-Final-Project"): cwd=cwd.parent
+for _ in range(5): 
+    if cwd.name!="STAT-587-Final-Project":
+        cwd=cwd.parent
+    else:
+        break
+else:
+    raise FileNotFoundError("Could not find correct workspace folder.")
 
 # Hyperparameters
 lag=[1, 3, 7, 14]
@@ -114,27 +124,124 @@ X=X.drop(columns=['Target'], level=0)
 
 print("Final shape:", X.shape[0], "rows,", X.shape[1], "columns.")
 
-X_train, X_test, yr_train, yr_test = train_test_split(X, y_regression, test_size=0.1)
-RFRegression = RandomForestRegressor(max_depth=1000, max_features=1000, n_jobs=-1)
-RFRegression.fit(X_train, yr_train)
+X_train, X_test, yr_train, yr_test=train_test_split(X, y_regression, test_size=0.2)
+Xc_train, Xc_test, yc_train, yc_test=train_test_split(X, y_classification, test_size=0.2)
 
-predictions = RFRegression.predict(X_test)
-prediction_direction = (pd.Series(predictions) >= 0).astype(int).to_numpy()
-print("Average predicted direction:", np.mean(prediction_direction))
-yr_test = (yr_test >= 0).astype(int).to_numpy()
+# ------- Generic Random Forest Regression Model ------------------
+# print("------- Random Forest Regression -------")
+# RFRegressor = RandomForestRegressor(max_depth=10, max_features=1000, n_jobs=-1)
+# RFRegressor.fit(X_train, yr_train)
 
-accuracy = np.mean(prediction_direction == yr_test)
-print("Accuracy (*100%):", accuracy * 100)
+# RF_predictions = RFRegressor.predict(X_test)
+# RF_prediction_direction = (pd.Series(RF_predictions) >= 0).astype(int).to_numpy()
+# print("Average predicted direction:", np.mean(RF_prediction_direction))
+# yr_test = (yr_test >= 0).astype(int).to_numpy()
 
-RFRegression_feature_df = pd.DataFrame({
-    'Feature': X_train.columns,
-    'Importance': RFRegression.feature_importances_
-}).sort_values(by='Importance', ascending=False)
-RFRegression_feature_df.head(50).plot(kind='barh', x="Feature", y="Importance")
-plt.xlabel("Feature Importance")
-plt.xticks(rotation=45)
-plt.ylabel("Feature Name")
-plt.show()
+# accuracy = np.mean(RF_prediction_direction == yr_test)
+# print("Accuracy (*100%):", accuracy * 100)
 
-print(X.columns.get_level_values(0).unique())
+# RFRegressor_feature_df=pd.DataFrame({
+#     'Feature': X_train.columns,
+#     'Importance': RFRegressor.feature_importances_
+# }).sort_values(by='Importance', ascending=False)
+# RFRegressor_feature_df.head(50).plot(kind='barh', x="Feature", y="Importance")
+# plt.xlabel("Feature Importance")
+# plt.xticks(rotation=45)
+# plt.ylabel("Feature Name")
+# plt.show()
+# print("------- ------- -------")
+#-------------------------
+
+# ------- Principal Component Analysis --------
+print("------- Principal Component Analysis -------")
+scaler = StandardScaler()
+X_train_std=scaler.fit_transform(X_train)
+X_test_std=scaler.transform(X_test)
+Xc_train_std=scaler.fit_transform(Xc_train)
+Xc_test_std=scaler.transform(Xc_test)
+
+pca=PCA(n_components=0.90)
+X_train_pca=pca.fit_transform(X_train_std)
+X_test_pca=pca.transform(X_test_std)
+Xc_train_pca=pca.fit_transform(Xc_train_std)
+Xc_test_pca=pca.transform(Xc_test_std)
+
+pca_columns=[f'PC {i}' for i in range(X_train_pca.shape[1])]
+X_train_pca=pd.DataFrame(X_train_pca, columns=pca_columns, index=X_train.index)
+X_test_pca=pd.DataFrame(X_test_pca, columns=pca_columns, index=X_test.index)
+
+pca_columns=[f'PC {i}' for i in range(Xc_train_pca.shape[1])]
+Xc_train_pca=pd.DataFrame(Xc_train_pca, columns=pca_columns, index=Xc_train.index)
+Xc_test_pca=pd.DataFrame(Xc_test_pca, columns=pca_columns, index=Xc_test.index)
+
+print("Reduced shape (Regression):", X_train_pca.shape[0] + X_test_pca.shape[0], "rows,", X_train_pca.shape[1], "columns.")
+print("Reduced shape (Classification):", Xc_train_pca.shape[0] + Xc_test_pca.shape[0], "rows,", Xc_train_pca.shape[1], "columns.")
+print("------- ------- -------")
+# ------------------------
+
+# ------- Random Forest Regression (reduced) -------
+# print("------- Random Forest Regression (Reduced) -------")
+RFRegressor_red=RandomForestRegressor(n_jobs=-1)
+# RFRegressor_red.fit(X_train_pca, yr_train)
+
+# RF_red_predictions=RFRegressor_red.predict(X_test_pca)
+# RF_red_prediction_direction = (pd.Series(RF_red_predictions) >= 0).astype(int).to_numpy()
+# print("Average predicted direction:", np.mean(RF_red_prediction_direction))
+# yr_test=(yr_test >= 0).astype(int).to_numpy()
+
+# accuracy=np.mean(RF_red_prediction_direction == yr_test)
+# print("Accuracy (*100%):", accuracy * 100)
+
+# RFRegressor_red_feature_df=pd.DataFrame({
+#     'Feature': X_train_pca.columns,
+#     'Importance': RFRegressor_red.feature_importances_
+# }).sort_values(by='Importance', ascending=False)
+# RFRegressor_red_feature_df.head(50).plot(kind='barh', x="Feature", y="Importance")
+# plt.xlabel("Feature Importance")
+# plt.xticks(rotation=45)
+# plt.ylabel("Feature Name")
+# plt.show()
+# print("------- ------- -------")
+# ------------------------
+
+# ------- Random Forest Classification (Reduced)
+# print("------- Random Forest Classifier (Reduced) -------")
+RFClassifier_red=RandomForestClassifier(n_jobs=-1)
+# RFClassifier_red.fit(Xc_train_pca, yc_train)
+
+# RFC_red_predictions=RFClassifier_red.predict(Xc_test_pca)
+# print("Average predicted direction:", np.mean(RFC_red_predictions))
+
+# accuracy=np.mean(RFC_red_predictions == yc_test)
+# print("Accuracy (*100%):", accuracy * 100)
+
+# RFClassifier_red_feature_df=pd.DataFrame({
+#     'Feature': Xc_train_pca.columns,
+#     'Importance': RFClassifier_red.feature_importances_
+# }).sort_values(by='Importance', ascending=False)
+# RFClassifier_red_feature_df.head(50).plot(kind='barh', x="Feature", y="Importance")
+# plt.xlabel("Feature Importance")
+# plt.xticks(rotation=45)
+# plt.ylabel("Feature Name")
+# plt.show()
+# print("------- ------- -------")
+# ------------------------
+
+# tscv=TimeSeriesSplit(n_splits=3)
+# sfs=SequentialFeatureSelector(RFRegression, n_features_to_select=10, direction='forward', cv=tscv)
+
+# ------- Stepwise Regression -------
+mfs=MFS(RFRegressor_red, k_features=int(np.sqrt(X.shape[0])), forward=True, floating=True, cv=3, n_jobs=-1)
+mfs.fit(X_train_pca, yr_train)
+X_train_stepwise=mfs.transform(X_train_pca)
+X_test_stepwise=mfs.transform(X_test_pca)
+
+mfs=MFS(RFClassifier_red, k_features=int(np.sqrt(X.shape[0])), forward=True, floating=True, cv=3, n_jobs=-1)
+mfs.fit(Xc_train_pca, yc_train)
+Xc_train_stepwise=mfs.transform(Xc_train_pca)
+Xc_test_stepwise=mfs.transform(Xc_test_pca)
+# ------------------------
+
+
+# print(X.columns.get_level_values(0).unique())
 

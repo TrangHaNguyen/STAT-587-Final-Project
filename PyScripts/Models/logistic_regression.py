ddcaa16f70a25efb4aa5aef9200304a6d8fe6313
Takeshi Stormer <takeshi.stormer@gmail.com>
2026-03-03 00:50:24 -0800

    Fully implemented pipelines; updated model_evaluation.py methods; updated dimensionality reduction methods in dimension_reduction.py; formatted code and added print statements for better visibility.

--- PyScripts/Models/logistic_regression.py
+++ PyScripts/Models/logistic_regression.py
@@ -3,53 +3,42 @@ from sklearn.linear_model import LogisticRegressionCV, LogisticRegression
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 from sklearn.model_selection import train_test_split, TimeSeriesSplit, GridSearchCV
-from data_preprocessing_and_cleaning import clean_data
-# from model_evaluation import display_feat_importances_logistic, classification_wfv_eval, classification_accuracy
-# from dimension_reduction import RIDGE, apply_PCA, LASSO, step_wise_reg_wfv
-
 from sklearn.pipeline import Pipeline
 
+from data_preprocessing_and_cleaning import clean_data
+
 '''No need for hyperparameter tuning for Logistic Regression via GridSearchCV since LogisticRegressionCV performs internal CV to select the best C value. We will just use the default 10 values of C that LogisticRegressionCV tests.'''
+
+VERBOSE=2
 
 if __name__=="__main__":
     X, y_regression=cast(Any, clean_data())
     X_train, X_test, y_train, y_test=train_test_split(X, y_regression, test_size=0.2, random_state=1)
     def to_binary_class(y):
         return (y>=0).astype(int).to_numpy()
+    y_regression=to_binary_class(y_regression)
     y_train=to_binary_class(y_train)
     y_test=to_binary_class(y_test)
     tscv=TimeSeriesSplit(n_splits=3)
 
     # ------- LASSO(Internal) APPLICATION -------
-    Log_Reg_R=LogisticRegressionCV(Cs=10, cv=tscv, penalty='l1', solver='saga', random_state=1, n_jobs=-1, max_iter=500, tol=1e-2, verbose=3)
+    Log_Reg_R=LogisticRegressionCV(Cs=10, cv=tscv, penalty='l1', solver='saga', random_state=1, n_jobs=-1, max_iter=500, tol=1e-2, verbose=VERBOSE)
     
     Log_Reg_model_pipeline_R = Pipeline([('scaler', StandardScaler()), ('classifier', Log_Reg_R)])
 
     Log_Reg_model_pipeline_R.fit(X_train, y_train)
 
-    # acc, avg_dir=classification_accuracy(Log_Reg_model_pipeline_R.predict(X_test), y_test)
-    # print("Accuracy (Test):", acc)
-    # print("Average Direction (Test):", avg_dir)
-
-    # display_feat_importances_logistic(Log_Reg_model_pipeline_R.named_steps['classifier'], X_train)
-
-    # classification_wfv_eval(Log_Reg_model_pipeline_R, X_train, y_train)
     input("Press Enter to continue...")
+
     # ------- RIDGE(Internal) APPLICATION -------
-    Log_Reg_L=LogisticRegressionCV(Cs=10, cv=tscv, penalty='l2', solver='saga', random_state=1, n_jobs=-1, max_iter=500, tol=1e-2, verbose=3)
+    Log_Reg_L=LogisticRegressionCV(Cs=10, cv=tscv, penalty='l2', solver='saga', random_state=1, n_jobs=-1, max_iter=500, tol=1e-2, verbose=VERBOSE)
     
     Log_Reg_model_pipeline_L=Pipeline([('scaler', StandardScaler()), ('classifier', Log_Reg_L)])
 
     Log_Reg_model_pipeline_L.fit(X_train, y_train)
 
-    # acc, avg_dir=classification_accuracy(Log_Reg_model_pipeline_L.predict(X_test), y_test)
-    # print("Accuracy (Test):", acc)
-    # print("Average Direction (Test):", avg_dir)
-
-    # display_feat_importances_logistic(Log_Reg_model_pipeline_L.named_steps['classifier'], X_train)
-
-    # classification_wfv_eval(Log_Reg_model_pipeline_L, X_train, y_train)
     input("Press Enter to continue...")
+
     # ------- PCA to Ridge(Internal) APPLICATION -------
     Log_Reg_PCA_L=LogisticRegression(penalty='l2', solver='liblinear', random_state=1)
     
@@ -61,15 +50,8 @@ if __name__=="__main__":
         'pca__n_components': [0.7, 0.8, 0.9, 0.95],
         'classifier__C': [0.01, 0.1, 1.0, 10.0]     
     }
-    grid_search_PCA_ridge=GridSearchCV(Log_Reg_model_pipeline_PCA_L, param_grid, cv=tscv, return_train_score=True, verbose=3)
+    grid_search_PCA_ridge=GridSearchCV(Log_Reg_model_pipeline_PCA_L, param_grid, cv=tscv, return_train_score=True, verbose=VERBOSE)
 
     grid_search_PCA_ridge.fit(X_train, y_train)
 
-    # acc, avg_dir=classification_accuracy(Log_Reg_model_pipeline_PCA_L.predict(X_test), y_test)
-    # print("Accuracy (Test):", acc)
-    # print("Average Direction (Test):", avg_dir)
-
-    # display_feat_importances_logistic(Log_Reg_model_pipeline_PCA_L.named_steps['classifier'], X_train)
-
-    # classification_wfv_eval(Log_Reg_model_pipeline_PCA_L, X_train, y_train)
     input("Press Enter to Finish...")
--- PyScripts/Models/random_forest.py
+++ PyScripts/Models/random_forest.py
@@ -6,21 +6,26 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.decomposition import PCA
 from sklearn.feature_selection import SelectFromModel
-from dimension_reduction import step_wise_reg_wfv
-# from model_evaluation import classification_accuracy, display_feat_importances, classification_wfv_eval
-from data_preprocessing_and_cleaning import clean_data
 from sklearn.pipeline import Pipeline
+
+from dimension_reduction import step_wise_reg_wfv
+from data_preprocessing_and_cleaning import clean_data
+from model_evaluation import get_final_metrics_grid, rolling_window_backtest, classification_accuracy
+
+VERBOSE=0
 
 if __name__=="__main__":
     X, y_regression=cast(Any, clean_data())
     X_train, X_test, y_train, y_test=train_test_split(X, y_regression, test_size=0.2, random_state=1)
     def to_binary_class(y):
         return (y>=0).astype(int)
+    y_regression=to_binary_class(y_regression)
     y_train=to_binary_class(y_train)
     y_test=to_binary_class(y_test)
     tscv=TimeSeriesSplit(n_splits=3)
     
-    # ------- BASE APPLICATION -------
+    # # ------- BASE APPLICATION -------
+    # print("\n\n------- Base RF Model -------")
     # RFClassifier_base=RandomForestClassifier(random_state=1, n_jobs=-1)
 
     # RF_pipeline_base=Pipeline([('scaler', StandardScaler()), 
@@ -30,19 +35,17 @@ if __name__=="__main__":
     #     'classifier__max_depth': [2, 3, 5],
     #     'classifier__n_estimators': [250, 500]
     # }
-    # grid_search_base=GridSearchCV(RF_pipeline_base, param_grid, cv=tscv, n_jobs=-1, return_train_score=True, verbose=3)
+    # grid_search_base=GridSearchCV(RF_pipeline_base, param_grid, cv=tscv, n_jobs=-1, return_train_score=True, verbose=VERBOSE)
     # grid_search_base.fit(X_train, y_train)
 
-    # display_feat_importances(RF_pipeline_base, X_train)
+    # rolling_window_backtest(grid_search_base, X, y_regression, verbose=1)
 
-    # acc, avg_dir=classification_accuracy(RF_pipeline_base.predict(X_test), y_test)
-    # print("Accuracy (Test):", acc)
-    # print("Average Direction (Test):", avg_dir)
+    # get_final_metrics_grid(grid_search_base, X_test, y_test)
 
-    # classification_wfv_eval(RF_pipeline_base, X_train, y_train)
+    # input("Press Enter to continue...")
 
-    input("Press Enter to continue...")
-    # ------- PCA APPLICATION -------
+    # # ------- PCA APPLICATION -------
+    # print("\n\n------- PCA RF Model -------")
     # RFClassifier_PCA=RandomForestClassifier(random_state=1, n_jobs=-1)
 
     # RF_pipeline_PCA=Pipeline([('scaler', StandardScaler()),
@@ -54,20 +57,18 @@ if __name__=="__main__":
     #     'classifier__max_depth': [2, 3, 5],
     #     'classifier__n_estimators': [250, 500]
     # }
-    # grid_search_PCA=GridSearchCV(RF_pipeline_PCA, param_grid, cv=tscv, n_jobs=-1, return_train_score=True, verbose=3)
+    # grid_search_PCA=GridSearchCV(RF_pipeline_PCA, param_grid, cv=tscv, n_jobs=-1, return_train_score=True, verbose=VERBOSE)
     # grid_search_PCA.fit(X_train, y_train)
 
-    # display_feat_importances(RFClassifier_red_PCA, X_train)
+    # rolling_window_backtest(grid_search_PCA, X, y_regression, verbose=1)
 
-    # acc, avg_dir=classification_accuracy(RFClassifier_red_PCA.predict(X_test), y_test)
-    # print("Accuracy (Test):", acc)
-    # print("Average Direction (Test):", avg_dir)
+    # get_final_metrics_grid(grid_search_PCA, X_test, y_test)
 
-    # classification_wfv_eval(RFClassifier_red_PCA, X_train, y_train)
+    # input("Press Enter to continue...")
 
-    input("Press Enter to continue...")
-    # ------- LASSO APPLICATION -------
-    # lasso_selector=SelectFromModel(LogisticRegression(penalty='l1', solver='liblinear', random_state=1), threshold='mean')
+    # # ------- LASSO APPLICATION -------
+    # print("\n\n------- LASSO RF Model -------")
+    # lasso_selector=SelectFromModel(LogisticRegression(l1_ratio=1, solver='saga', random_state=1, max_iter=500, tol=5e-2), threshold='mean')
     # RFClassifier_red_lasso=RandomForestClassifier(random_state=1, n_jobs=-1)
 
     # RF_pipeline_lasso=Pipeline([('scaler', StandardScaler()), 
@@ -79,46 +80,41 @@ if __name__=="__main__":
     #     'classifier__max_depth': [2, 3, 5],              
     #     'classifier__n_estimators': [500]
     # }
-    # grid_search_LASSO=GridSearchCV(RF_pipeline_lasso, param_grid, cv=tscv, n_jobs=-1, return_train_score=True, verbose=3)
+    # grid_search_LASSO=GridSearchCV(RF_pipeline_lasso, param_grid, cv=tscv, n_jobs=-1, return_train_score=True, verbose=VERBOSE)
     # grid_search_LASSO.fit(X_train, y_train)
 
-    # display_feat_importances(RFClassifier_red_LASSO, X_train)
+    # rolling_window_backtest(grid_search_LASSO, X, y_regression, verbose=1)
 
-    # acc, avg_dir=classification_accuracy(RFClassifier_red_LASSO.predict(X_test), y_test)
-    # print("Accuracy (Test):", acc)
-    # print("Average Direction (Test):", avg_dir)
+    # get_final_metrics_grid(grid_search_LASSO, X_test, y_test)
 
-    # classification_wfv_eval(RFClassifier_red_LASSO, X_train, y_train)
+    # input("Press Enter to continue...")
 
-    input("Press Enter to continue...")
-    # ------- RIDGE APPLICATION -------
-    # ridge_selector=SelectFromModel(LogisticRegression(penalty='l2', solver='liblinear', random_state=1), threshold='mean')
+    # # ------- RIDGE APPLICATION -------
+    # print("\n\n------- RIDGE RF Model -------")
+    # ridge_selector=SelectFromModel(LogisticRegression(l1_ratio=0, solver="saga", random_state=1, max_iter=500, tol=5e-2), threshold='mean')
     # RFClassifier_red_ridge=RandomForestClassifier(random_state=1, n_jobs=-1)
 
     # RF_pipeline_ridge=Pipeline([('scaler', StandardScaler()), 
     #                           ('feature_selector', ridge_selector),
     #                           ('classifier', RFClassifier_red_ridge)])
 
-    
     # param_grid={
     #     'feature_selector__estimator__C': [0.001, 0.01, 0.1],
     #     'classifier__max_depth': [2, 3, 5],              
     #     'classifier__n_estimators': [500]
     # }
-    # grid_search_ridge=GridSearchCV(RF_pipeline_ridge, param_grid, cv=tscv, n_jobs=-1, return_train_score=True, verbose=3)
+    # grid_search_ridge=GridSearchCV(RF_pipeline_ridge, param_grid, cv=tscv, n_jobs=-1, return_train_score=True, verbose=VERBOSE)
     # grid_search_ridge.fit(X_train, y_train)
 
-    # display_feat_importances(RFClassifier_red_RIDGE, X_train)
+    # rolling_window_backtest(grid_search_ridge, X, y_regression, verbose=1)
 
-    # acc, avg_dir=classification_accuracy(RFClassifier_red_RIDGE.predict(X_test), y_test)
-    # print("Accuracy (Test):", acc)
-    # print("Average Direction (Test):", avg_dir)
+    # get_final_metrics_grid(grid_search_ridge, X_test, y_test)
 
-    # classification_wfv_eval(RFClassifier_red_RIDGE, X_train, y_train)
+    # input("Press Enter to continue...")
 
-    input("Press Enter to continue...")
     # ------- STEP-WISE REGRESSION APPLICATION -------
-    lasso_selector=SelectFromModel(LogisticRegression(penalty='l1', solver='liblinear', random_state=1), threshold='mean')
+    print("\n\n------- LASSO(internal) -> STEP-WISE REGRESSION RF Model -------")
+    lasso_selector=SelectFromModel(LogisticRegression(l1_ratio=1, solver='saga', random_state=1, max_iter=500, tol=5e-2), max_features=100, threshold='mean')
     RFClassifier_red_lasso=RandomForestClassifier(random_state=1, n_jobs=-1)
 
     RF_pipeline_lasso=Pipeline([('scaler', StandardScaler()), 
@@ -130,7 +126,7 @@ if __name__=="__main__":
         'classifier__max_depth': [2, 3, 5],              
         'classifier__n_estimators': [500]
     }
-    grid_search_LASSO=GridSearchCV(RF_pipeline_lasso, param_grid, cv=tscv, n_jobs=-1, return_train_score=True, verbose=3)
+    grid_search_LASSO=GridSearchCV(RF_pipeline_lasso, param_grid, cv=tscv, n_jobs=-1, return_train_score=True, verbose=VERBOSE)
     grid_search_LASSO.fit(X_train, y_train) 
 
     best_params_from_grid = grid_search_LASSO.best_params_
@@ -150,14 +146,12 @@ if __name__=="__main__":
 
     X_train_final, X_test_final=step_wise_reg_wfv(RFClassifier_red_sw_wfv, X_train_red, y_train, X_test_red) 
 
-    RFClassifier_red_sw_wfv.fit(X_train_final, X_test_final)
+    RFClassifier_red_sw_wfv.fit(X_train_final, y_train)
 
-    # display_feat_importances(RFClassifier_red_sw_wfv, X_train)
+    rolling_window_backtest(RFClassifier_red_sw_wfv, X[X_train_final.columns], y_regression, verbose=1)
 
-    # acc, avg_dir=classification_accuracy(RFClassifier_red_sw_wfv.predict(X_test), y_test)
-    # print("Accuracy (Test):", acc)
-    # print("Average Direction (Test):", avg_dir)
-
-    # classification_wfv_eval(RFClassifier_red_sw_wfv, X_train, y_train)
+    acc, avg=classification_accuracy(RFClassifier_red_sw_wfv.predict(X_test_final), y_test)
+    print("Accuracy (Test):", acc)
+    print("Average Direction:", avg)
 
     input("Press Enter to Finish...")
